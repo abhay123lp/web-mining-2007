ccc3ead9ec4773332894c874354ef966c2778613
michel.salim <michel.salim@39cd418f-7e28-0410-938f-61386eee0065>
2007-02-10 23:01:44 +0000

    Add current database schema. Note: link type and weight not added.
    Type should go into the links table; not certain if weights should be kept
    in the database or just kept in the serialized data dump of the specific
    algorithms that generate it

---
+++ trunk/doc/schemas/links.sql
@@ -0,0 +1,13 @@
+/* Create the links table
+*/
+
+CREATE TABLE `links` (
+  `srcid` int(11) NOT NULL default '0',
+  `destid` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`srcid`,`destid`),
+  KEY `destid` (`destid`),
+  CONSTRAINT `links_ibfk_2` FOREIGN KEY (`destid`)
+    REFERENCES `blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `links_ibfk_1` FOREIGN KEY (`srcid`)
+    REFERENCES `blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

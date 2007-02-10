16b5865a59a677abef8d91c92ab4b69cec9eb305
michel.salim <michel.salim@39cd418f-7e28-0410-938f-61386eee0065>
2007-02-10 19:27:54 +0000

    Commit initial link analysis and database-interface code

---
+++ analyzer/edu/indiana/cs/webmining/util/DBEngine.java
@@ -0,0 +1,93 @@
+package edu.indiana.cs.webmining.util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+public class DBEngine {
+
+	private static Connection conn;
+
+	public static Connection getConnection() throws SQLException {
+		if (conn == null) {
+			try {
+				// Load MySQL connector
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+				FileInputStream propstream = new FileInputStream("etc/login.prop");
+				Properties prop = new Properties();
+				prop.load(propstream);
+				propstream.close();
+
+				String connStr = "jdbc:mysql://localhost/blogmining?user="
+					+ prop.getProperty("user")
+					+ "&password="
+					+ prop.getProperty("password");
+				System.out.println("connStr=" + connStr);
+				conn = DriverManager.getConnection(connStr);
+			} catch (Exception e) {
+				throw new SQLException("Connection unavailable");
+			}
+		}
+		return conn;
+	}
+
+	private static Iterable<String> getResults(final ResultSet rs, final int coln) {
+		return new Iterable<String>() {
+			public Iterator<String> iterator() {
+				return new Iterator<String>() {
+					private String next;
+					public boolean hasNext() {
+						if (next == null) {
+							try {
+								if (! rs.next()) {
+									return false;
+								}
+								next = rs.getString(coln);
+							} catch (SQLException e) {
+								System.err.println("Something wrong in ResultSet");
+								return false;
+							}
+						}
+						return true;
+					}
+					public String next() {
+						if (! hasNext()) {
+							throw new NoSuchElementException();
+						}
+						String retval = next;
+						next = null;
+						return retval;
+					}
+					
+					public void remove() {
+						throw new UnsupportedOperationException("Remove not allowed");
+					}
+				};
+				
+			}
+		};
+	}
+	public static String[] getSuccessors(String url) throws SQLException {
+		String[] res;
+		Statement stmt = getConnection().createStatement();
+		String connStr = "SELECT url FROM links AS l JOIN blogs AS b "
+			+ "ON l.destid = b.id "
+			+ "WHERE srcid = (SELECT id FROM blogs "
+			+ "WHERE url='" + url + "');";
+		ResultSet rs = stmt.executeQuery(connStr);
+		ArrayList<String> tres = new ArrayList<String>();
+		for (String s_url: getResults(rs, 1)) {
+			tres.add(s_url);
+		}
+		res = new String[tres.size()];
+		return tres.toArray(res);
+	}
+}

e1a7bacf1ded65c5b3314d08bd3edddaeb7b537b
eran.chinthaka <eran.chinthaka@39cd418f-7e28-0410-938f-61386eee0065>
2007-02-11 05:54:30 +0000

    Some minor improvements.

--- trunk/analyzer/edu/indiana/cs/webmining/util/JungTest.java
+++ trunk/analyzer/edu/indiana/cs/webmining/util/JungTest.java
@@ -1,101 +1,97 @@
 package edu.indiana.cs.webmining.util;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-
 import edu.uci.ics.jung.algorithms.importance.HITS;
 import edu.uci.ics.jung.algorithms.importance.NodeRanking;
-import edu.uci.ics.jung.graph.Edge;
 import edu.uci.ics.jung.graph.Vertex;
 import edu.uci.ics.jung.graph.impl.DirectedSparseEdge;
 import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.impl.SimpleDirectedSparseVertex;
 import edu.uci.ics.jung.utils.UserData;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
 public class JungTest {
 
-	static DirectedSparseGraph graph;
-	static HashMap<String, Vertex> urlVertexMap;
-	static HashMap<Vertex, String> vertexURLMap;
-	/**
-	 * @param args
-	 */
-	
-	private static Vertex makeVertex(String url) {
-		Vertex v = new SimpleDirectedSparseVertex();
-		v.addUserDatum("url", url, UserData.CLONE);
-		urlVertexMap.put(url, v);
-		vertexURLMap.put(v, url);
-		graph.addVertex(v);
-		return v;
-	}
+    static DirectedSparseGraph graph;
+    static HashMap<String, Vertex> urlVertexMap;
+    static HashMap<Vertex, String> vertexURLMap;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		if (args.length < 1) {
-			System.err.println("Usage: java JungTest blogurl");
-			return;
-		}
-		
-		String srcurl = args[0];
-		
-		try {
-			String[] Dm1urls = DBEngine.getPredecessors(srcurl);
-			String[] D1urls = DBEngine.getSuccessors(srcurl);
-			urlVertexMap = new HashMap<String, Vertex>();
-			vertexURLMap = new HashMap<Vertex, String>();
+    private static Vertex makeVertex(String url) {
+        Vertex v = new SimpleDirectedSparseVertex();
+        v.addUserDatum("url", url, UserData.CLONE);
+        urlVertexMap.put(url, v);
+        vertexURLMap.put(v, url);
+        graph.addVertex(v);
+        return v;
+    }
 
-			graph = new DirectedSparseGraph();
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-			// Add edges from source to D1
-			Vertex v1 = makeVertex(srcurl);
-			for (String url: D1urls) {
-				Vertex dv = makeVertex(url);
-				graph.addEdge(new DirectedSparseEdge(v1, dv));
-			}
-			// Add edges from Dm1 to source
-			for (String url: Dm1urls) {
-				Vertex sv = makeVertex(url);
-				graph.addEdge(new DirectedSparseEdge(sv, v1));
-			}
-			
-			// Add edges from D1 to D2
-			for (String url: D1urls) {
-				Vertex sv = urlVertexMap.get(url);
-				for (String next: DBEngine.getSuccessors(url)) {
-					Vertex dv = makeVertex(next);
-					graph.addEdge(new DirectedSparseEdge(sv, dv));
-				}
-			}
-			// Add edges from Dm2 to Dm1
-			for (String url: Dm1urls) {
-				Vertex dv = urlVertexMap.get(url);
-				for (String prev: DBEngine.getPredecessors(url)) {
-					Vertex sv = makeVertex(prev);
-					graph.addEdge(new DirectedSparseEdge(sv, dv));
-				}
-			}
-			
-			System.out.println("Adjacency graph has "
-					+ graph.numVertices() + " vertices and "
-					+ graph.numEdges() + " edges");
-			HITS hits = new HITS(graph);
-			hits.evaluate();
-			List<NodeRanking> authorities = (List<NodeRanking>)hits.getRankings();
-			int i=0;
-			for (NodeRanking nr: authorities) {
-				// Only print top 20 ranks
-				if (i++ >= 20) break;
-				System.out.println("" + vertexURLMap.get(nr.vertex)
-						+ "\t" + nr.rankScore);
-			}
-			//hits.printRankings(true, true);
-		} catch (SQLException e) {
-			System.err.println("Database failure: " + e.getMessage());
-		}
-		
-	}
+        if (args.length < 1) {
+            System.err.println("Usage: java JungTest blogurl");
+            return;
+        }
+
+        String srcurl = args[0];
+
+        try {
+            String[] Dm1urls = DBEngine.getPredecessors(srcurl);
+            String[] D1urls = DBEngine.getSuccessors(srcurl);
+            urlVertexMap = new HashMap<String, Vertex>();
+            vertexURLMap = new HashMap<Vertex, String>();
+
+            graph = new DirectedSparseGraph();
+
+            // Add edges from source to D1
+            Vertex v1 = makeVertex(srcurl);
+            for (String url : D1urls) {
+                Vertex dv = makeVertex(url);
+                graph.addEdge(new DirectedSparseEdge(v1, dv));
+            }
+            // Add edges from Dm1 to source
+            for (String url : Dm1urls) {
+                Vertex sv = makeVertex(url);
+                graph.addEdge(new DirectedSparseEdge(sv, v1));
+            }
+
+            // Add edges from D1 to D2
+            for (String url : D1urls) {
+                Vertex sv = urlVertexMap.get(url);
+                for (String next : DBEngine.getSuccessors(url)) {
+                    Vertex dv = makeVertex(next);
+                    graph.addEdge(new DirectedSparseEdge(sv, dv));
+                }
+            }
+            // Add edges from Dm2 to Dm1
+            for (String url : Dm1urls) {
+                Vertex dv = urlVertexMap.get(url);
+                for (String prev : DBEngine.getPredecessors(url)) {
+                    Vertex sv = makeVertex(prev);
+                    graph.addEdge(new DirectedSparseEdge(sv, dv));
+                }
+            }
+
+            System.out.println("Adjacency graph has "
+                    + graph.numVertices() + " vertices and "
+                    + graph.numEdges() + " edges");
+            HITS hits = new HITS(graph);
+            hits.evaluate();
+            List<NodeRanking> authorities = (List<NodeRanking>) hits.getRankings();
+            int i = 0;
+            for (NodeRanking nr : authorities) {
+                // Only print top 20 ranks
+                if (i++ >= 20) break;
+                System.out.println("" + vertexURLMap.get(nr.vertex)
+                        + "\t" + nr.rankScore);
+            }
+            //hits.printRankings(true, true);
+        } catch (SQLException e) {
+            System.err.println("Database failure: " + e.getMessage());
+        }
+
+    }
 
 }

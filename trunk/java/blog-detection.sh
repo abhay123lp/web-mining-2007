403a04b14a6ba5d440d0a0ef3dd4af2a206c2617
eran.chinthaka <eran.chinthaka@39cd418f-7e28-0410-938f-61386eee0065>
2007-04-03 03:38:16 +0000

    more changes

--- trunk/java/blog-detection.sh
+++ trunk/java/blog-detection.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 BLOG_CLASSPATH=""
-for f in "lib/*.jar
+for f in lib/*.jar
 do
-  BLOG_CLASSPATH="BLOG_CLASSPATH":$f
+  BLOG_CLASSPATH="$BLOG_CLASSPATH":$f
 done
 BLOG_CLASSPATH="$JAVA_HOME/lib/tools.jar":"$BLOG_CLASSPATH":build/crawler.jar:"$CLASSPATH"
 
-$JAVA_HOME/bin/java -Xmx2048m -classpath "$BLOG_CLASSPATH" edu.indiana.cs.webmining.blog.BlogProcessingSystem
+echo $BLOG_CLASSPATH
+
+$JAVA_HOME/bin/java -Xmx2048m -cp "$BLOG_CLASSPATH" edu.indiana.cs.webmining.blog.BlogProcessingSystem

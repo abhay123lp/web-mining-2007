5aebbc29e7c6dcffd540dfa17fcd1c5954fe5a28
eran.chinthaka <eran.chinthaka@39cd418f-7e28-0410-938f-61386eee0065>
2007-04-11 05:52:13 +0000

    Reducing heap space

--- trunk/java/start-blog-processors.sh
+++ trunk/java/start-blog-processors.sh
@@ -9,4 +9,4 @@ BLOG_CLASSPATH="$JAVA_HOME/lib/tools.jar":"$BLOG_CLASSPATH":build/crawler.jar:"$
 
 echo $BLOG_CLASSPATH
 
-$JAVA_HOME/bin/java -Xmx2048m -cp "$BLOG_CLASSPATH" edu.indiana.cs.webmining.blog.BlogProcessingSystem Blog
+$JAVA_HOME/bin/java -Xmx1024m -cp "$BLOG_CLASSPATH" edu.indiana.cs.webmining.blog.BlogProcessingSystem Blog

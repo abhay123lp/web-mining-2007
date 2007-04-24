9fa2be2202c5de2622d5f6749923cf9f9e566d14
michel.salim <michel.salim@39cd418f-7e28-0410-938f-61386eee0065>
2007-04-24 14:00:06 +0000

    Synchronization fixes (NOT the big database update yet)

--- java/src/spider/util/Redirections.java
+++ java/src/spider/util/Redirections.java
@@ -7,44 +7,50 @@ import java.util.Hashtable;
 
 /**
  * maintains a static datastructure of redirections
- *
+ * 
  * @author Gautam Pant
  */
 public class Redirections {
     private static Hashtable redirect = new Hashtable();
 
+    private static Object lock = new Object();
+
     /**
      * add a redirection entry to the Hashtable
      */
-    public synchronized static boolean addElement(String url1, String url2) {
-        if (url1 != null && url2 != null) {
-            redirect.put(url1, url2);
-            return true;
-        } else {
-            return false;
+    public static boolean addElement(String url1, String url2) {
+        synchronized (lock) {
+            if (url1 != null && url2 != null) {
+                redirect.put(url1, url2);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
     /**
      * write redirection data collected into the given filename
      */
-    public synchronized static void toFile(String filename) {
+    public static void toFile(String filename) {
         try {
-            String[] parts = filename.split("/+|\\+");
-            String path = Helper.join("/", parts, 0, parts.length - 2);
-            File f = new File(path);
-            if (!f.exists()) {
-                f.mkdir();
+            synchronized (lock) {
+                String[] parts = filename.split("/+|\\+");
+                String path = Helper.join("/", parts, 0, parts.length - 2);
+                File f = new File(path);
+                if (!f.exists()) {
+                    f.mkdir();
+                }
+                FileWriter fw = new FileWriter(new File(filename));
+                for (Enumeration e = redirect.keys(); e.hasMoreElements();) {
+                    String url1 = (String) e.nextElement();
+                    String url2 = (String) redirect.get(url1);
+                    fw.write(url1 + "  " + url2 + "\n");
+                }
+                fw.flush();
+                fw.close();
+                redirect = new Hashtable();
             }
-            FileWriter fw = new FileWriter(new File(filename));
-            for (Enumeration e = redirect.keys(); e.hasMoreElements();) {
-                String url1 = (String) e.nextElement();
-                String url2 = (String) redirect.get(url1);
-                fw.write(url1 + "  " + url2 + "\n");
-            }
-            fw.flush();
-            fw.close();
-            redirect = new Hashtable();
         } catch (Exception e) {
             return;
         }
@@ -53,18 +59,22 @@ public class Redirections {
     /**
      * get the redirected location
      */
-    public synchronized static String getLocation(String URL) {
-        if (redirect.containsKey(URL)) {
-            return (String) redirect.get(URL);
-        } else {
-            return null;
+    public static String getLocation(String URL) {
+        synchronized (lock) {
+            if (redirect.containsKey(URL)) {
+                return (String) redirect.get(URL);
+            } else {
+                return null;
+            }
         }
     }
 
     /**
      * get size of resirections hash
      */
-    public synchronized static int size() {
-        return redirect.size();
-	}		
+    public static int size() {
+        synchronized (lock) {
+            return redirect.size();
+        }
+    }
 }
